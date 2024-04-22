ca302075cec11e031ae7b23ffdcb5ee59f541bcb
Andreas <apn21017@student.mdu.se>
2024-04-22 12:03:16 +0200

    First Commit

---
+++ MainTest.cpp
@@ -0,0 +1,188 @@
+#include <chrono>
+#include <cmath>
+#include <future>
+#include <iostream>
+#include <thread>
+
+#include <mavsdk/mavsdk.h>
+#include <mavsdk/plugins/action/action.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
+#include <mavsdk/plugins/offboard/offboard.h>
+
+using mavsdk::Mavsdk;
+using mavsdk::ConnectionResult;
+using mavsdk::Offboard;
+using mavsdk::Action;
+using mavsdk::Telemetry;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::this_thread::sleep_for;
+
+bool offb_ctrl_body(mavsdk::Offboard& offboard);
+void usage(const std::string& bin_name);
+
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        usage(argv[0]);
+        return 1;
+    }
+
+    Mavsdk mavsdk{Mavsdk::Configuration{Mavsdk::ComponentType::GroundStation}};
+    ConnectionResult connection_result = mavsdk.add_any_connection(argv[1]);
+
+    if (connection_result != ConnectionResult::Success) {
+        std::cerr << "Connection failed: " << connection_result << '\n';
+        return 1;
+    }
+
+    auto system = mavsdk.first_autopilot(3.0);
+    if (!system) {
+        std::cerr << "Timed out waiting for system\n";
+        return 1;
+    }
+
+    // Instantiate plugins.
+    auto action = Action{system.value()};
+    auto offboard = Offboard{system.value()};
+    auto telemetry = Telemetry{system.value()};
+
+    while (!telemetry.health_all_ok()) {
+        std::cout << "Waiting for system to be ready\n";
+        sleep_for(seconds(1));
+    }
+    std::cout << "System is ready\n";
+
+    const auto arm_result = action.arm();
+    if (arm_result != Action::Result::Success) {
+        std::cerr << "Arming failed: " << arm_result << '\n';
+        return 1;
+    }
+    std::cout << "Armed\n";
+
+    const auto takeoff_result = action.takeoff();
+    if (takeoff_result != Action::Result::Success) {
+        std::cerr << "Takeoff failed: " << takeoff_result << '\n';
+        return 1;
+    }
+
+    auto in_air_promise = std::promise<void>{};
+    auto in_air_future = in_air_promise.get_future();
+    Telemetry::LandedStateHandle handle = telemetry.subscribe_landed_state(
+        [&telemetry, &in_air_promise, &handle](Telemetry::LandedState state) {
+            if (state == Telemetry::LandedState::InAir) {
+                std::cout << "Taking off has finished\n.";
+                telemetry.unsubscribe_landed_state(handle);
+                in_air_promise.set_value();
+            }
+        });
+    in_air_future.wait_for(seconds(10));
+    if (in_air_future.wait_for(seconds(3)) == std::future_status::timeout) {
+        std::cerr << "Takeoff timed out.\n";
+        return 1;
+    }
+
+    //  using body co-ordinates
+    if (!offb_ctrl_body(offboard)) {
+        return 1;
+    }
+
+    const auto land_result = action.land();
+    if (land_result != Action::Result::Success) {
+        std::cerr << "Landing failed: " << land_result << '\n';
+        return 1;
+    }
+
+    // Check if vehicle is still in air
+    while (telemetry.in_air()) {
+        std::cout << "Vehicle is landing...\n";
+        sleep_for(seconds(1));
+    }
+    std::cout << "Landed!\n";
+
+    // We are relying on auto-disarming but let's keep watching the telemetry for
+    // a bit longer.
+    sleep_for(seconds(3));
+    std::cout << "Finished...\n";
+
+    return 0;
+}
+
+bool offb_ctrl_body(mavsdk::Offboard& offboard)
+{
+    std::cout << "Starting Offboard velocity control in body coordinates\n";
+
+    // Send it once before starting offboard, otherwise it will be rejected.
+    Offboard::VelocityBodyYawspeed stay{};
+    offboard.set_velocity_body(stay);
+
+    Offboard::Result offboard_result = offboard.start();
+    if (offboard_result != Offboard::Result::Success) {
+        std::cerr << "Offboard start failed: " << offboard_result << '\n';
+        return false;
+    }
+    std::cout << "Offboard started\n";
+
+	float deg_s_speed;
+    std::cout << "Turn clock-wise and climb\n";
+	std::cout << "Enter rotational speed: ";
+	std::cin >> deg_s_speed;
+    Offboard::VelocityBodyYawspeed cc_and_climb{};
+    cc_and_climb.down_m_s = -1.0f;
+	cc_and_climb.forward_m_s;
+	cc_and_climb.right_m_s;
+    cc_and_climb.yawspeed_deg_s = deg_s_speed;
+    offboard.set_velocity_body(cc_and_climb);
+    sleep_for(seconds(5));
+
+    std::cout << "Turn back anti-clockwise\n";
+    Offboard::VelocityBodyYawspeed ccw{};
+    ccw.down_m_s = -1.0f;
+    ccw.yawspeed_deg_s = -60.0f;
+    offboard.set_velocity_body(ccw);
+    sleep_for(seconds(5));
+
+    std::cout << "Wait for a bit\n";
+    offboard.set_velocity_body(stay);
+    sleep_for(seconds(2));
+
+    std::cout << "Fly a circle\n";
+    Offboard::VelocityBodyYawspeed circle{};
+    circle.forward_m_s = 5.0f;
+    circle.yawspeed_deg_s = 30.0f;
+    offboard.set_velocity_body(circle);
+    sleep_for(seconds(15));
+
+    std::cout << "Wait for a bit\n";
+    offboard.set_velocity_body(stay);
+    sleep_for(seconds(5));
+
+    std::cout << "Fly a circle sideways\n";
+    circle.right_m_s = -5.0f;
+    circle.yawspeed_deg_s = 30.0f;
+    offboard.set_velocity_body(circle);
+    sleep_for(seconds(15));
+
+    std::cout << "Wait for a bit\n";
+    offboard.set_velocity_body(stay);
+    sleep_for(seconds(8));
+
+    offboard_result = offboard.stop();
+    if (offboard_result != Offboard::Result::Success) {
+        std::cerr << "Offboard stop failed: " << offboard_result << '\n';
+        return false;
+    }
+    std::cout << "Offboard stopped\n";
+
+    return true;
+}
+
+void usage(const std::string& bin_name)
+{
+    std::cerr << "Usage : " << bin_name << " <connection_url>\n"
+              << "Connection URL format should be :\n"
+              << " For TCP : tcp://[server_host][:server_port]\n"
+              << " For UDP : udp://[bind_host][:bind_port]\n"
+              << " For Serial : serial:///path/to/serial/dev[:baudrate]\n"
+              << "For example, to connect to the simulator use URL: udp://:14540\n";
+}
