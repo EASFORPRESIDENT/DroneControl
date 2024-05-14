0327a9213af2108d99180bbe9c6475ede7a623e7
Fredrik Johansson <fjn20007@student.mdu.se>
2024-05-14 13:35:20 +0200

    added z-axis input and outputs

--- DQN/dqn.py
+++ DQN/dqn.py
@@ -95,8 +95,8 @@ class DQN(nn.Module):
         return x
 
 # Define some hyperparameters
-input_size = 3  # x and y positions
-output_size = 5  # Number of possible actions
+input_size = 4  # x and y positions
+output_size = 7  # Number of possible actions
 learning_rate = 0.001
 gamma = 0.99  # Discount factor
 epsilon = 1.0  # Exploration rate
@@ -207,7 +207,11 @@ class Environment:
 
         distance_to_target = np.sqrt(X_pos**2+Y_pos**2)
 
-        reward =  max(0, 1 - distance_to_target / max_distance)  
+        reward_one =  max(0, 1 - distance_to_target / max_distance)
+
+        reward_two = max(0,1 - abs((10-posZ))/5)
+
+        reward = 0.7*reward_one + 0.3*reward_two
 
         if distance_to_target >= max_distance or steps >= max_steps or posZ < 5 or posZ > 15:
             done =  1 
