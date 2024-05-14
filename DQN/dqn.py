879a4534e5bd37ffdc50942d0adba0de2429b718
Fredrik Johansson <fjn20007@student.mdu.se>
2024-05-14 11:12:14 +0200

    sleep in send action test

--- DQN/dqn.py
+++ DQN/dqn.py
@@ -71,6 +71,7 @@ def sharedMemorySend(action):
     mapped_send = mmap.mmap(memory_s.fd, memory_s.size)
 
     action = 1
+    time.sleep(0.5)
     action_to_send = struct.pack('i?', action, RunLoop)
 
     mapped_send.write(action_to_send)
